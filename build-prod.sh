667e82a08053b0abf09ac65ef154905a467116fa
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2019-04-11 18:59:50 +0700

    android build script

--- build-prod.sh
+++ build-prod.sh
@@ -25,7 +25,7 @@ ionic cordova build android --release --prod
 
 APKDir=platforms/android/app/build/outputs/apk/release
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore stars-release-key.jks ${APKDir}/app-release-unsigned.apk stars-alias
-rm ${APKDir}/starsexpert.apk
+rm ${APKDir}/starsexpert.apk || true
 zipalign -v 4 ${APKDir}/app-release-unsigned.apk ${APKDir}/starsexpert.apk
 apksigner verify ${APKDir}/starsexpert.apk
 aapt dump badging ${APKDir}/starsexpert.apk | grep version
